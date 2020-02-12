22c2446b4a694e9042d88134747899859c3ab079
liwenliang <liwenliang>
2020-02-12 19:49:49 +0800

    你能做到吗？

---
+++ precache-manifest.6686c61e72036e911d00484cc62a1933.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "629daabbd00d8dca1195fb84234af5bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "688dcdee30282cb479a8",
+    "url": "/static/css/2.215d1bc9.chunk.css"
+  },
+  {
+    "revision": "d6f06e8d6a3ea9be496d",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "688dcdee30282cb479a8",
+    "url": "/static/js/2.f3d38fcb.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/static/js/2.f3d38fcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6f06e8d6a3ea9be496d",
+    "url": "/static/js/main.b1808847.chunk.js"
+  },
+  {
+    "revision": "c84ce3372f2e04cba055",
+    "url": "/static/js/runtime-main.e575be5b.js"
+  },
+  {
+    "revision": "6eb26c0fd0e44da89380aba5eed32ef3",
+    "url": "/static/media/avatar.6eb26c0f.jpg"
+  },
+  {
+    "revision": "e59de9b20a4d455b2017a5382b1a5897",
+    "url": "/static/media/border.e59de9b2.png"
+  }
+]);
